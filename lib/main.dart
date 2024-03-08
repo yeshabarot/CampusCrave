ca905a18fa731cae2530320c97c90ac2247894fc
Deep1012 <deepmehta012@gmail.com>
2024-03-08 22:05:10 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:campuscrave/firebase_options.dart';
-import 'package:campuscrave/pages/home.dart';
-import 'package:campuscrave/screens/login_screen.dart';
+import 'package:campuscrave/pages/details.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -18,6 +17,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LoginScreen());
+    return const MaterialApp(home: Details());
   }
 }
