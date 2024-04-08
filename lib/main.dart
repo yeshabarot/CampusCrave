86531e4344b359892a12e414231d57ff0bdf7e5c
Deep1012 <deepmehta012@gmail.com>
2024-04-09 00:23:27 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {        
     return const MaterialApp(home: WelcomeScreen());
   }
 }
