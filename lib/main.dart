2526183cc8ef45d0256982da1055e29d37ebfd84
Yakshraj Dalwadi <yakshdalwadi1002@gmail.com>
2024-04-17 15:36:14 +0530

    yooo

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,50 @@
-import 'package:campuscrave/admin/admin_login.dart';
+import 'package:campuscrave/constants/colors.dart';
 import 'package:campuscrave/firebase_options.dart';
-import 'package:campuscrave/screens/login_screen.dart';
-import 'package:campuscrave/screens/signin_screen.dart';
+import 'package:campuscrave/pages/bottomnav.dart';
+import 'package:campuscrave/pages/order.dart';
+import 'package:campuscrave/pages/success.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:campuscrave/data/repositories/authentication_repository.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// class DatabaseMethods {
+//   // Define other methods in your DatabaseMethods class
+
+//   Future<void> updateFoodCartQuantity(
+//       String userId, String itemId, int quantity) async {
+//     try {
+//       await FirebaseFirestore.instance
+//           .collection('users')
+//           .doc(userId)
+//           .collection('foodCart')
+//           .doc(itemId)
+//           .update({
+//         'Quantity': quantity,
+//       });
+//     } catch (e) {
+//       print('Error updating quantity: $e');
+//     }
+//   }
+// }
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
   );
+
+  // initializing local storage
+  await GetStorage.init();
+
+  // native Splash screen
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(const MyApp());
 }
 
@@ -17,7 +52,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {        
-    return const MaterialApp(home: LoginScreen());
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: const Scaffold(
+          backgroundColor: CCcolors.primary,
+          body: Center(child: CircularProgressIndicator(color: Colors.white))),
+    );
   }
 }
