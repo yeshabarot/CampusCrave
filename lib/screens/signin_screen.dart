96d2152ab84aad46b5d9543bace73d81eabdcf4f
Yakshraj Dalwadi <yakshdalwadi1002@gmail.com>
2024-04-17 15:35:21 +0530

    Update signin_screen.dart

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,242 +1,160 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:campuscrave/pages/bottomnav.dart';
 import 'package:campuscrave/screens/login_screen.dart';
 import 'package:campuscrave/services/database.dart';
 import 'package:campuscrave/services/shared_pref.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:random_string/random_string.dart';
 
 class SignUpScreen extends StatefulWidget {
-  const SignUpScreen({Key? key}) : super(key: key);
+  const SignUpScreen({super.key});
 
   @override
   State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  String email = "", password = "", name = "";
 
-  String email = "";
-  String password = "";
-  String name = "";
+  TextEditingController namecontroller = new TextEditingController();
 
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordcontroller = new TextEditingController();
 
-  final _formKey = GlobalKey<FormState>();
+  TextEditingController mailcontroller = new TextEditingController();
 
-  bool _passwordVisible = false;
+  final _formkey = GlobalKey<FormState>();
 
-  void _togglePasswordVisibility() {
-    setState(() {
-      _passwordVisible = !_passwordVisible;
-    });
-  }
+  registration() async {
+    if (password != null) {
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password);
 
-  Future<void> _signInWithGoogle() async {
-    try {
-      await _googleSignIn.signOut(); // Sign out of current Google session
+        ScaffoldMessenger.of(context).showSnackBar((const SnackBar(
+            backgroundColor: Color.fromARGB(255, 22, 210, 50),
+            content: Text(
+              "Registered Successfully",
+              style: TextStyle(fontSize: 20.0, color: Colors.black),
+            ))));
 
-      final GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
-      if (googleSignInAccount != null) {
-        final GoogleSignInAuthentication googleSignInAuthentication =
-            await googleSignInAccount.authentication;
-
-        final AuthCredential credential = GoogleAuthProvider.credential(
-          accessToken: googleSignInAuthentication.accessToken,
-          idToken: googleSignInAuthentication.idToken,
-        );
-
-        final UserCredential userCredential = await _auth.signInWithCredential(credential);
-        final User? user = userCredential.user;
-
-        if (user != null) {
-          // Signed in
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Sign in with Google succeeded'),
-            ),
-          );
-
-          // Save user information to shared preferences
-          await SharedPreferenceHelper().saveUserName(user.displayName ?? '');
-          await SharedPreferenceHelper().saveUserEmail(user.email ?? '');
-          await SharedPreferenceHelper().saveUserId(user.uid);
-
-          // Navigate to the home page after successful sign-in
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => const BottomNav()),
-          );
-        } else {
-          // Handle sign-in failure
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Failed to sign in with Google'),
-            ),
-          );
+        String Id = randomAlphaNumeric(10);
+        Map<String, dynamic> addUserInfo = {
+          "Name": namecontroller.text,
+          "Email": mailcontroller.text,
+          "Id": Id,
+        };
+        await DatabaseMethods().addUserDetail(addUserInfo, Id);
+        await SharedPreferenceHelper().saveUserName(namecontroller.text);
+        await SharedPreferenceHelper().saveUserEmail(mailcontroller.text);
+        await SharedPreferenceHelper().saveUserId(Id);
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => BottomNav()));
+      } on FirebaseException catch (e) {
+        if (e.code == 'weak-password') {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              backgroundColor: Colors.orangeAccent,
+              content: Text(
+                "Password Provided is too Weak",
+                style: TextStyle(fontSize: 18.0),
+              )));
+        } else if (e.code == "email-already-in-use") {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              backgroundColor: Colors.orangeAccent,
+              content: Text(
+                "Account Already exsists",
+                style: TextStyle(fontSize: 18.0),
+              )));
         }
       }
-    } catch (e) {
-      print(e.toString());
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Failed to sign in with Google'),
-        ),
-      );
-    }
-  }
-
-  void _register() async {
-    // Perform registration logic
-    // Replace with your registration logic
-    try {
-      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
-            "Registered Successfully",
-            style: TextStyle(fontSize: 20.0),
-          ),
-        ),
-      );
-
-      String Id = randomAlphaNumeric(10);
-      Map<String, dynamic> addUserInfo = {
-        "Name": nameController.text,
-        "Email": emailController.text,
-        "Id": Id,
-      };
-      await DatabaseMethods().addUserDetail(addUserInfo, Id);
-      await SharedPreferenceHelper().saveUserName(nameController.text);
-      await SharedPreferenceHelper().saveUserEmail(emailController.text);
-      await SharedPreferenceHelper().saveUserId(Id);
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const BottomNav()),
-      );
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text(
-              "Password Provided is too Weak",
-              style: TextStyle(fontSize: 18.0),
-            ),
-          ),
-        );
-      } else if (e.code == "email-already-in-use") {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text(
-              "Account Already Exists",
-              style: TextStyle(fontSize: 18.0),
-            ),
-          ),
-        );
-      }
-    } catch (e) {
-      print(e.toString());
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+        child: Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+
+          //column
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            //Section 1
+            const Image(
+                image: AssetImage("images/SigninIMG.png"),
+                width: 200,
+                height: 240),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Get On Board!",
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.w800),
+              ),
+            ),
+            const Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Create your account to start Ordering!",
+                    style: TextStyle(fontSize: 20))),
+            const SizedBox(
+              height: 10,
+            ),
+            //section 2
+            signinForm(),
+            //section 3
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Image(
-                  image: AssetImage("images/SigninIMG.png"),
-                  width: 200,
-                  height: 240,
-                ),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Get On Board!",
-                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.w800),
-                  ),
-                ),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Create your account to start Ordering!",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
+                const Text("OR"),
                 const SizedBox(
-                  height: 10,
+                  height: 0,
                 ),
-                signinForm(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text("OR"),
-                    const SizedBox(
-                      height: 0,
+                const SizedBox(height: 10),
+                SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: OutlinedButton.icon(
+                    icon: const Image(
+                      image: AssetImage("images/Google.png"),
+                      width: 30.0,
+                      height: 30,
                     ),
-                    SizedBox(
-                      height: 50,
-                      width: double.infinity,
-                      child: OutlinedButton.icon(
-                        icon: const Image(
-                          image: AssetImage("images/Google.png"),
-                          width: 30.0,
-                          height: 30,
-                        ),
-                        onPressed: _signInWithGoogle,
-                        label: const Text(
-                          "Sign-in With Google",
-                          style: TextStyle(fontSize: 15),
-                        ),
-                      ),
+                    onPressed: () {},
+                    label: const Text(
+                      "Sign-in With Google",
+                      style: TextStyle(fontSize: 15),
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
+                  ),
+                ),
+                const SizedBox(height: 15),
+                GestureDetector(
+                    onTap: () {
+                      Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => const LoginScreen(),
-                          ),
-                        );
-                      },
-                      child: const Text(
-                        "Already have an account? Log In",
-                        style: TextStyle(fontSize: 15, color: Colors.blue),
-                      ),
-                    ),
-                  ],
-                ),
+                              builder: (context) => const LoginScreen()));
+                    },
+                    child: const Text(
+                      "Already have an account? Log In",
+                      style: TextStyle(fontSize: 15, color: Colors.blue),
+                    ))
               ],
             ),
-          ),
+          ]),
         ),
       ),
-    );
+    ));
   }
 
   Form signinForm() {
     return Form(
-      key: _formKey,
+      key: _formkey,
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: .0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextFormField(
-              controller: nameController,
+              controller: namecontroller,
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Please Enter Name';
@@ -244,17 +162,16 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 return null;
               },
               decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.person_outline_outlined),
-                labelText: "Full Name ",
-                hintText: "Eg. Deep",
-                border: OutlineInputBorder(),
-              ),
+                  prefixIcon: Icon(Icons.person_outline_outlined),
+                  labelText: "Full Name ",
+                  hintText: "Eg. Deep",
+                  border: OutlineInputBorder()),
             ),
             const SizedBox(
               height: 15,
             ),
             TextFormField(
-              controller: emailController,
+              controller: mailcontroller,
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Please Enter E-mail';
@@ -262,31 +179,30 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 return null;
               },
               decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.email_outlined),
-                labelText: "Email",
-                hintText: "example@gmail.com",
-                border: OutlineInputBorder(),
-              ),
+                  prefixIcon: Icon(Icons.email_outlined),
+                  labelText: "Email",
+                  hintText: "example@gmail.com",
+                  border: OutlineInputBorder()),
             ),
             const SizedBox(
               height: 15,
             ),
             TextFormField(
-              controller: passwordController,
+              controller: passwordcontroller,
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Please Enter Password';
                 }
                 return null;
               },
-              obscureText: !_passwordVisible,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.fingerprint),
+              obscureText: true,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.fingerprint),
                 labelText: "Password",
                 border: OutlineInputBorder(),
                 suffixIcon: IconButton(
-                  onPressed: _togglePasswordVisibility,
-                  icon: Icon(_passwordVisible ? Icons.visibility : Icons.visibility_off),
+                  onPressed: null,
+                  icon: Icon(Icons.remove_red_eye),
                 ),
               ),
             ),
@@ -295,23 +211,23 @@ class _SignUpScreenState extends State<SignUpScreen> {
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
+                  if (_formkey.currentState!.validate()) {
                     setState(() {
-                      email = emailController.text;
-                      name = nameController.text;
-                      password = passwordController.text;
+                      email = mailcontroller.text;
+                      name = namecontroller.text;
+                      password = passwordcontroller.text;
                     });
                   }
-                  _register();
+                  registration();
                 },
                 style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-                  backgroundColor: const Color.fromARGB(226, 0, 0, 0),
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 40), // function to chnage padding
+                    backgroundColor: const Color.fromARGB(226, 0, 0, 0),
+                    foregroundColor: Colors.white, // textcolor
+                    shape: LinearBorder.bottom() //border color and style
+                    ),
                 child: const Text("SIGN UP"),
               ),
             ),
